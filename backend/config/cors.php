b02584a75632d197d212aa7c7ccf15c71c4edefe
Ria Choi <riiachoii@gmail.com>
2026-03-14 01:31:48 +0900

    frontend

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:3000', 'https://next-aim-website.onrender.com'],
 
     'allowed_origins_patterns' => [],
 
