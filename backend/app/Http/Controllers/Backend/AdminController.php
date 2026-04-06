e9b869cd42e478a89f12965a03546a9460a9ad0c
Ria Choi <riiachoii@gmail.com>
2026-04-06 22:13:25 +0900

    419 Admin Login Error Fixed try2

--- backend/app/Http/Controllers/Backend/AdminController.php
+++ backend/app/Http/Controllers/Backend/AdminController.php
@@ -33,7 +33,7 @@ class AdminController extends Controller
         if (Auth::guard('admin')->attempt($credentials)) {
             $request->session()->regenerate();
 
-            return redirect()->route('admin.dashboard');
+            return redirect()->to(route('admin.dashboard', [], false));
         }
 
         return back()->withErrors([
@@ -69,7 +69,7 @@ class AdminController extends Controller
 
         Auth::guard('admin')->login($admin);
 
-        return redirect()->route('admin.dashboard');
+        return redirect()->to(route('admin.dashboard', [], false));
     }
 
     /**
@@ -82,6 +82,6 @@ class AdminController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerate();
 
-        return redirect()->route('admin.login');
+        return redirect()->to(route('admin.login', [], false));
     }
 }
